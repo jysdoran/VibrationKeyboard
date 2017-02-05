ebad08093d7588ffdcf926cc8d831f870b59c4fa
Harshil <harshil.r.joshi@gmail.com>
2017-02-05 08:29:44 +0000

    included API, not specific to area yet

--- src/USBUtils/Reader.java
+++ src/USBUtils/Reader.java
@@ -1,9 +1,48 @@
 package USBUtils;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.imageio.ImageIO;
+// // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-public class Reader {
+public class JavaSample
+{
+    public static void main(String[] args)
+    {
+        HttpClient httpclient = HttpClients.createDefault();
 
+        try
+        {
+            URIBuilder builder = new URIBuilder([https://api.projectoxford.ai/vision/v1.0/ocr]);
+
+            builder.setParameter("optimalcharacterrecognition", "Categories");
+
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", "{CognitiveServicesApiKeys.ComputerVision.rawValue}");
+
+
+            // Request body
+            StringEntity reqEntity = new StringEntity("{body}");
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                System.out.println(EntityUtils.toString(entity));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 }
